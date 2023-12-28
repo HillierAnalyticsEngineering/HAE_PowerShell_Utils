5f9fadd1c326dce2d11cc870cf554801ed25c18a
Andy Hillier <84726034+HillierAnalyticsEngineering@users.noreply.github.com>
2023-12-28 10:21:57 -0500

    Add files via upload

--- PowerShell Script/FileMigrationUtilityScript.ps1
+++ PowerShell Script/FileMigrationUtilityScript.ps1
@@ -8,6 +8,7 @@ $defaultDaysOffset = $config.DefaultDaysOffset
 # Gets latest timestamp and path to log file
 $latestTransferTimestamp = [DateTime]::MinValue
 $logFilePath = "$PSScriptRoot\LogFile.txt"
+$timestampFilePath = "$PSScriptRoot\TimeStampFile.txt"
 # Recursively iterate through the source parent directory and get a list of file paths
 $files = Get-ChildItem -Path $sourceParentDirectory -Recurse | Where-Object { $_.PSIsContainer -eq $false } | Select-Object -ExpandProperty FullName
 # Create an array to store objects of source and destination filepath pairs
@@ -30,8 +31,8 @@ foreach ($file in $files) {
 }
 
 
-# Get the timestamp of the last file transfer from the log file
-$lastTransferTimestamp = Get-Content -Path $logFilePath -ErrorAction SilentlyContinue
+# Get the timestamp of the last file transfer from the timestamp file
+$lastTransferTimestamp = Get-Content -Path $timestampFilePath -ErrorAction SilentlyContinue
 Write-Host $lastTransferTimestamp
 Write-Host (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
 # If the log file contains a timestamp, set it as the start date for Robocopy
@@ -41,33 +42,131 @@ if ($lastTransferTimestamp -ne $null) {
     $DaysOffset = $ts.Days
 } else {
     # If the log file is empty, get items modified based on default days offset in config file
-    $DaysOffset = $defaultDaysOffset
+    [int]$DaysOffset = [convert]::ToInt32($defaultDaysOffset, 10)
 }
 
+# If there is no archive folder, make one, otherwise do nothing
+if (Test-Path -Path "$PSScriptRoot\Archive") {
+    # Do Nothing
+} else {
+    New-Item -Path "$PSScriptRoot\Archive" -ItemType Directory
+}
+$archiveCurrentDatePath = "$PSScriptRoot\Archive\" + (Get-Date).ToString("yyyy-MM-dd")
+
+# Empty Log File
+'' | Out-File $logFilePath
+$todayDate = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+"Script Initiated on $todayDate.`n`nBelow is a detailed summary of robocopy file migration jobs completed:`n`n" | Out-File -append $logFilePath
 
 # Iterate through each source and destination pair in the object array '$paths' created above
 #     ROBOCOPY DOCS: https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
-foreach ($path in $paths) {
-    $sourcePath = $path.SourcePath
-    $destinationPath = $path.DestinationPath
-    $file = $path.File
 
-    # Define Robocopy options (see robocopy docs above for more explanation)
-    # /COPY:DAT says to copy timestamps from directories
-    # /DCOPY:T gets timestamps of directories
-    # /R:1 means to retry once
-    # /W:1 means to wait one second between retries
-    # /V means to produce the verbose (detailed) output
-    # /TEE says to write the status output to the console window, and to the log file.
-    # /MAXAGE specifies the maximum file age (excludes files older than start date)
-    # /LOG+:$logFilePath says to append the status output to $logFilePath (".\LogFile.txt")
-    $robocopyOptions = "/MAXAGE:$DaysOffset /E /COPY:DAT /DCOPY:T /R:1 /W:1 /V /TEE /LOG+:`"$logFilePath`""
-    $robocopyCommand = "robocopy `"$sourcePath`" `"$destinationPath`" `"$file`" $robocopyOptions"
-    Invoke-Expression $robocopyCommand
-}
+# PARALLEL BATCH 1 - ARCHIVE (BACKUP) FILE TRANSFER ----------
+
+    $archiveJobs = @()
+
+    foreach ($path in $paths) {
+        $sourcePath = $path.SourcePath
+        $destinationPath = $path.DestinationPath
+        $file = $path.File
+        $archiveDirectorySubPath = $destinationPath -replace [regex]::Escape($destinationParentDirectory), ''
+        $archivePath = $archiveCurrentDatePath + $archiveDirectorySubPath
+
+        if ( ((Get-Date) - (ls ($sourcePath + '\\' + $file)).LastWriteTime).Days -lt $DaysOffset -or ((Get-Date) - (ls ($sourcePath + '\\' + $file)).LastWriteTime).Days -eq 0) {
+            #Add file to archive folder
+            $robocopyOptionsArchive = "/MAXAGE:$DaysOffset /COPY:DAT /DCOPY:T /R:1 /W:1 /V /TEE"
+            $robocopyCommandArchive = "robocopy `"$destinationPath`" `"$archivePath`" `"$file`" $robocopyOptions"
+            # create guid for job, and add guid to list of guids for getting job-data on completion
+            $id = [System.Guid]::NewGuid()
+            $archiveJobs += $id
+            # start robocopy as a background process (parallel processing) 
+            # Note that this awaits the associated main file transfer to complete (using guid)
+            Start-Job -Name $id -Scriptblock { Invoke-Expression $using:robocopyCommandArchive } 
+        }
+    }
+
+    # Basically, check every second to see if jobs are done yet.
+    While (Get-Job -State "Running") {
+        cls
+        Get-Job
+        Start-Sleep 1 
+    }
+    # Clear the host for brevity
+    cls
+    # Show completed job listing and write to terminal
+    Get-Job
+    write-host "`n`nArchive Jobs completed, Writing output . . .`n"
+
+    # Write all of the completed job info to the log file (waiting until all complete prevents thread-locking)
+    foreach($job in $archiveJobs) {
+        Receive-Job -Name $job | Out-File -Append $logFilePath
+    }
+
+    # Removes all jobs to ensure no jobs are still running among those started
+        foreach($job in $archiveJobs) {
+        Remove-Job -Name $job -ErrorAction SilentlyContinue
+    }
+
+# PARALLEL BATCH 2 - MAIN (REPLACEMENT OR ADDITION) FILE TRANSFER ----------
+
+    $jobs = @()
+
+    foreach ($path in $paths) {
+        $sourcePath = $path.SourcePath
+        $destinationPath = $path.DestinationPath
+        $file = $path.File
+        $archiveDirectorySubPath = $destinationPath -replace [regex]::Escape($destinationParentDirectory), ''
+        $archivePath = $archiveCurrentDatePath + $archiveDirectorySubPath
+
+        if ( ((Get-Date) - (ls ($sourcePath + '\\' + $file)).LastWriteTime).Days -lt $DaysOffset -or ((Get-Date) - (ls ($sourcePath + '\\' + $file)).LastWriteTime).Days -eq 0) {
+            # Define Robocopy options (see robocopy docs above for more explanation)
+            $robocopyOptions = "/E /MAXAGE:$DaysOffset /COPY:DAT /DCOPY:T /R:1 /W:1 /V /TEE"
+            $robocopyCommand = "robocopy `"$sourcePath`" `"$destinationPath`" `"$file`" $robocopyOptions"
+            # create guid for job, and add guid to list of guids for getting job-data on completion
+            $id1 = [System.Guid]::NewGuid()
+            $jobs += $id
+            # start robocopy as a background process (parallel processing)
+            Start-Job -Name $id -Scriptblock { Invoke-Expression $using:robocopyCommand }
+        }
+    }
+
+    # Basically, check every second to see if jobs are done yet.
+    While (Get-Job -State "Running") {
+        cls
+        Get-Job
+        Start-Sleep 1 
+    }
+    # Clear the host for brevity
+    cls
+    # Show completed job listing and write to terminal
+    Get-Job
+    write-host "`n`nJobs completed, writing output . . .`n"
+
+    # Write all of the completed job info to the log file (waiting until all complete prevents thread-locking)
+    foreach($job in $jobs) {
+        Receive-Job -Name $job | Out-File -Append $logFilePath
+    }
+
+    # Removes all jobs to ensure no jobs are still running among those started
+    foreach($job in $jobs) {
+        Remove-Job -Name $job -ErrorAction SilentlyContinue
+    }
 
 # Output the latest transfer timestamp to the console and update the log file
 $latestTransferTimestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-$latestTransferTimestamp | Out-File $logFilePath
+$latestTransferTimestamp | Out-File $timestampFilePath
+
+#Delete archive directories with dates older than '$backupDaysOffset' (Defined in JSON config file)
+[int]$backupDaysOffset = [convert]::ToInt32('-' + $config.BackupDaysOffset, 10)
+$checkDate = (Get-Date).AddDays($backupDaysOffset)
+Get-ChildItem -Path "$PSScriptRoot\Archive\" | ForEach-Object {
+    $content = ($_.PSPath -split '[\\]')[-1]
+    $contentDate = [DateTime]::ParseExact($content, "yyyy-MM-dd", $null)
+    if ($contentDate -lt $checkDate) {
+        Remove-Item -Path "$PSScriptRoot\Archive\$_" -Recurse 
+    }
+}
+
+"`n`n Script complete: `nLatest transfer timestamp written to LogFile: $latestTransferTimestamp" | Out-File -append $logFilePath
 Write-Host "`n`n Script complete: `nLatest transfer timestamp written to LogFile: $latestTransferTimestamp"
 Read-Host -Prompt "Press any key to exit . . ."
